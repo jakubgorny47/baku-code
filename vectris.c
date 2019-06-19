e865a88f5dc888e28f337686c3213d2b346016ce
jakubgorny47 <52002391+jakubgorny47@users.noreply.github.com>
2019-06-19 16:42:48 +0200

    C implementation of Cpp's Vector

---
+++ vectris.c
@@ -0,0 +1,102 @@
+#include "vectris.h"
+
+vectris vectris_init(unsigned int typeSize, unsigned int initialCapacity) {
+	int reserve = (initialCapacity ? 10 : 0);
+
+	vectris v = {NULL, initialCapacity, typeSize, reserve, 10};
+	vectris_allocate(&v, initialCapacity + reserve);
+	v._size = initialCapacity;
+
+	return v;
+}
+
+void vectris_allocate(vectris* v, unsigned int additionalSize) {
+	void* newData = realloc(v->_data, v->_typeSize * (v->_size + additionalSize));
+	
+	if (newData == NULL)
+		free(v->_data);
+	else
+		v->_extra = additionalSize;
+	v->_data = newData;
+}
+
+void vectris_deallocate(vectris* v, unsigned int substractSize) {
+	if (v->_size - substractSize < 1) {
+		free(v->_data);
+		v->_size = 0;
+		v->_extra = 0;
+		v->_data = NULL;
+
+		return;
+	}
+
+	void* newData = realloc(v->_data, v->_typeSize * (v->_size - substractSize + v->extra));
+	
+	if(newData == NULL)
+		free(v->_data);
+
+	v->_data = newData;
+	v->_size -= substractSize;
+}
+
+void vectris_clear(vectris* v) {
+	vectris_deallocate(v, v->_size);
+}
+
+void vectris_push(vectris* v, void* element) {
+	if (v->_extra < 1)
+		vectris_allocate(v, v->extra);
+
+	memcpy(v->_size * v->_typeSize + (unsigned int)v->_data, element, v->_typeSize);
+	v->_size++;
+	v->_extra--;
+}
+
+void vectris_pushi(vectris* v, long int number) {
+	vectris_push(v, &number);
+}
+
+void vectris_pushf(vectris* v, float number) {
+	vectris_push(v, &number);
+}
+
+void vectris_pushd(vectris* v, double number) {
+	vectris_push(v, &number);
+}
+
+void vectris_pop(vectris* v) {
+	if (v->_size < 1)
+		return;
+	vectris_deallocate(v, 1);
+	v->_extra++;
+}
+
+void* vectris_get(vectris* v, unsigned int index) {
+	if (v->_size < 1 || v->_size <= index)
+		return NULL;
+	return (void*)((unsigned int)v->_data + index * v->_typeSize);
+}
+
+void vectris_insert(vectris* v, unsigned int index, void* element) {
+	if (v->_extra < 1)
+		vectris_allocate(v, v->extra);
+
+	memmove((unsigned int)v->_data + index * v->_typeSize + v->_typeSize, (unsigned int)v->_data + index * v->_typeSize, (v->_size - index) * v->_typeSize);
+	memcpy((unsigned int)v->_data + index * v->_typeSize, element, v->_typeSize);
+}
+
+void vectris_inserti(vectris* v, unsigned int index, long int number) {
+	vectris_insert(v, index, &number);
+}
+
+void vectris_insertf(vectris* v, unsigned int index, float number) {
+	vectris_insert(v, index, &number);
+}
+
+void vectris_insertd(vectris* v, unsigned int index, double number) {
+	vectris_insert(v, index, &number);
+}
+
+void* vectris_remove(vectris* v, unsigned int index) {
+	
+}
